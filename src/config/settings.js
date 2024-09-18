e28c5af57d520164891a2dbf08029d9e31814483
Redemption <redemptionfi888@gmail.com>
2024-09-18 18:02:52 +0800

    Update settings.js

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,8 +1,8 @@
 export default {
-	title: `Ancourage Astro Theme | A Marketing Website Theme for Startups and Businesses`,
-	description: `A simple, clean, and modern theme for a startup or businesses' marketing website.`,
+	title: `Ancourage Astro Theme | Expert Tuition Services for Academic Success`,
+	description: `Ancourage provides expert tuition services for students of all levels. We offer personalized learning programs to help students excel in subjects like Math, English, and Science. Join us for academic success today!`,
 	url: `https://odyssey-theme.littlesticks.dev`, // No trailing slash!
-	name: `Odyssey`, // The short name of the business or brand name. Used for things like the copyright in the footer.
+	name: `Ancourage`, // The short name of the business or brand name. Used for things like the copyright in the footer.
   enableThemeSwitcher: true,
   showLittleSticksPlug: false, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
 };
